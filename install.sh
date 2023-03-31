6fca2c3711e1be72b8c674e864263afa3a750ed5
Tom Moyer <tom.moyer@uncc.edu>
2023-03-31 16:54:29 -0400

    Added more to the service file and fixed errors in the install script

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ cp ccd-server.service /etc/systemd/system/ccd-server.service
 systemctl daemon-reload
 
 mkdir -p /opt/ccd-server
-cp app.py requirements.txt /opt/ccd-server/app.py
+cp app.py requirements.txt /opt/ccd-server
 cp -a static templates /opt/ccd-server/
-python -m venv /opt/ccd-server/venv
+virtualenv /opt/ccd-server/venv
 pushd /opt/ccd-server
 . venv/bin/activate
-pip install < requirements.txt
+pip install -r /opt/ccd-server/requirements.txt
 deactivate
 popd
